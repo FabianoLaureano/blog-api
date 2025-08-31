3a321e100555fa61018fb5346b39db0be15168ce
FabianoLaureano <fabianoandrelaureano@gmail.com>
2025-08-31 18:32:02 -0300

    user model

--- controllers/v1/auth/register.ts
+++ controllers/v1/auth/register.ts
@@ -1,8 +1,14 @@
 import { logger } from "../../../lib/winston.ts";
 import config from "../../../config/index.ts";
 import type { Request, Response } from "express";
+import User from "../../../models/user.ts";
+import type { IUser } from "../../../models/user.ts";
+
+type UserData = Pick<IUser, "email" | "password" | "role">;
 
 const register = async (req: Request, res: Response): Promise<void> => {
+    const { email, password, role } = req.body as UserData;
+
   try {
     res.status(201).json({
       message: "New user created",
