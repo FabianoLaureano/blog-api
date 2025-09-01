72504dce29f50614936f76244e472114bb97a889
FabianoLaureano <fabianoandrelaureano@gmail.com>
2025-09-01 00:15:23 -0300

    criptografia do password

--- models/user.ts
+++ models/user.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
 export interface IUser {
   username: string;
@@ -84,5 +85,13 @@ const userSchema = new Schema<IUser>(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 export default model<IUser>("User", userSchema);
