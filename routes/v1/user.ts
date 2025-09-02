b57495ad9169f1e3d9891d74079ecc279f739952
FabianoLaureano <fabianoandrelaureano@gmail.com>
2025-09-02 01:18:52 -0300

    update user

--- routes/v1/user.ts
+++ routes/v1/user.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import { param, query, body } from "express-validator";
 import authenticate from "../../middlewares/authenticate.ts";
 import validationError from "../../middlewares/validationError.ts";
-//import User from "../../models/User.ts";
+import User from "../../models/user.ts";
 import authorize from "../../middlewares/authorize.ts";
 import getCurrentUser from "../../controllers/v1/user/get-current-user.ts";
+import updateCurrentUser from "../../controllers/v1/user/update-current-user.ts";
 
 const router = Router();
 
@@ -13,6 +14,67 @@ router.get(
   authenticate,
   authorize(["admin", "user"]),
   getCurrentUser
+);
+
+router.put(
+  "/current",
+  authenticate,
+  authorize(["admin", "user"]),
+  body("username")
+    .optional()
+    .trim()
+    .isLength({ max: 20 })
+    .withMessage("Username must be less than 20 characters")
+    .custom(async (value) => {
+      const userExists = await User.exists({ username: value });
+
+      if (userExists) {
+        throw new Error("Username already exists");
+      }
+    }),
+
+  body("email")
+    .optional()
+    .trim()
+    .isLength({ max: 50 })
+    .withMessage("Email must be less than 50 characters")
+    .isEmail()
+    .withMessage("Invalid email")
+    .custom(async (value) => {
+      const userExists = await User.exists({ email: value });
+
+      if (userExists) {
+        throw new Error("Email already in use");
+      }
+    }),
+
+  body("password")
+    .optional()
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters"),
+
+  body("first_name")
+    .optional()
+    .trim()
+    .isLength({ max: 20 })
+    .withMessage("First name must be less than 20 characters"),
+
+  body("last_name")
+    .optional()
+    .trim()
+    .isLength({ max: 20 })
+    .withMessage("Last name must be less than 20 characters"),
+
+  body(["website", "facebook", "instagram", "linkedin", "x", "youtube"])
+    .optional()
+    .isURL()
+    .withMessage("Invalid URL")
+    .isLength({ max: 100 })
+    .withMessage("URL must be less than 100 characters"),
+
+  validationError,
+  updateCurrentUser
 );
 
 export default router;
